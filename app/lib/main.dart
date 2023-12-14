7f99c2485c3febf0d586094276353095c9fddbd7
Lars Krämer <24323470+larskraemer@users.noreply.github.com>
2023-12-14 16:06:13 +0100

    Add App

---
+++ app/lib/main.dart
@@ -0,0 +1,199 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:robo_app/remoteControl.dart';
+import 'package:robo_app/selectDevicePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorSchemeSeed:const Color.fromARGB(255, 255, 0, 0),
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BluetoothState _btState = BluetoothState.UNKNOWN;
+  StreamSubscription<BluetoothDiscoveryResult>? _streamSubscription;
+  List<BluetoothDiscoveryResult> results =
+      List<BluetoothDiscoveryResult>.empty(growable: true);
+  bool isDiscovering = false;
+
+  String _address = "";
+  String _name = "";
+
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterBluetoothSerial.instance.state.then((value) {
+      setState(() {
+        _btState = value;
+      });
+    });
+
+    Future.doWhile(() async {
+      if ((await FlutterBluetoothSerial.instance.isEnabled) ?? false) {
+        return false;
+      }
+      await Future.delayed(const Duration(milliseconds: 100));
+      return true;
+    }).then((_) {
+      FlutterBluetoothSerial.instance.address.then((address) {
+        setState(() {
+          _address = address!;
+        });
+      });
+    });
+
+    FlutterBluetoothSerial.instance.name.then((value) {
+      setState(() {
+        _name = value!;
+      });
+    });
+
+    FlutterBluetoothSerial.instance
+        .onStateChanged()
+        .listen((BluetoothState state) {
+      setState(() {
+        _btState = state;
+      });
+    });
+  }
+
+  void _openRemoteControl(BluetoothDevice device) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+      return RemoteControlPage(server: device);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: ListView(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          children: <Widget>[
+            Divider(),
+            ListTile(title: const Text("General")),
+            SwitchListTile(
+                title: const Text("Bluetooth enabled"),
+                value: _btState.isEnabled,
+                onChanged: (value) {
+                  future() async {
+                    if (value) {
+                      await FlutterBluetoothSerial.instance.requestEnable();
+                    } else {
+                      await FlutterBluetoothSerial.instance.requestDisable();
+                    }
+                  }
+
+                  future().then((_) {
+                    setState(() => {});
+                  });
+                }),
+            ListTile(
+              title: const Text("Status"),
+              subtitle: Text(_btState.toString()),
+            ),
+            ListTile(
+                title: ElevatedButton(
+              child: const Text("Connect to Device"),
+              onPressed: () async {
+                final device = await Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                  return SelectDevicePage();
+                }));
+                if(device != null){
+                  print("Selected paired device ${device.address}");
+                  _openRemoteControl(device);
+                }
+              },
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ app/lib/selectDevicePage.dart
@@ -0,0 +1,161 @@
+
+
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+class SelectDevicePage extends StatefulWidget {
+  SelectDevicePage({super.key});
+
+  @override
+  State<SelectDevicePage> createState(){
+    return _SelectDevicePage();
+  }
+}
+
+class _SelectDevicePage extends State<SelectDevicePage> {
+  var bondedDevices = List<BluetoothDevice>.empty(growable: true);
+  var otherDevices = List<BluetoothDevice>.empty(growable: true);
+  bool _isDiscovering = false;
+  bool _isBonding = false;
+  StreamSubscription<BluetoothDiscoveryResult>? _streamSubscription;
+
+  void _startDiscovery(){
+    setState(() {
+      bondedDevices.clear();
+      otherDevices.clear();
+      _isDiscovering = true;
+    });
+    _streamSubscription = FlutterBluetoothSerial.instance.startDiscovery().listen((r){
+      setState(() {
+        if(r.device.bondState.isBonded){
+          final index = bondedDevices.indexWhere((element) => element.address == r.device.address);
+          if(index >= 0) {
+            bondedDevices[index] = r.device;
+          }
+          else {
+            bondedDevices.add(r.device);
+          }
+        }
+        else {
+          final index = otherDevices.indexWhere((element) => element.address == r.device.address);
+          if(index >= 0) {
+            otherDevices[index] = r.device;
+          }
+          else {
+            otherDevices.add(r.device);
+          }
+        }
+      });
+    });
+    _streamSubscription?.onDone(() {
+      setState(() {
+        _isDiscovering = false;
+      });
+    });
+  }
+
+  void _stopDiscovery(){
+    setState(() {
+      _streamSubscription?.cancel();
+      _isDiscovering = false;
+    });
+  }
+
+  Future<bool?> _bondDevice(BluetoothDevice device) {
+    return FlutterBluetoothSerial.instance.bondDeviceAtAddress(device.address).timeout(Duration(seconds: 30));
+  }
+  
+  @override
+  void initState() {
+    super.initState();
+    _startDiscovery();
+  }
+
+  @override
+  void dispose(){
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(context) {
+    Widget makeDeviceTileEntry(device, onTap){
+      return ListTile(
+        title: Text(device.name == null? device.address : "${device.name} [${device.address}]"),
+        onTap: onTap,
+      );
+    }
+    List<Widget> bondedEntries = bondedDevices.map((e) => makeDeviceTileEntry(e, (){
+      Navigator.of(context).pop(e);
+    })).toList();
+    List<Widget> unbondedEntries = otherDevices.map((e) => makeDeviceTileEntry(e, () async {
+      if(_isBonding) return;
+      _isBonding = true;
+      try{
+        final paired = (await _bondDevice(e))!;
+          if(paired) {
+            print("Pairing successful!");
+          }
+          else {
+            print("Pairing failed!");
+          }
+      }
+      catch(e) {
+
+      }
+    })).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: <Widget>[
+          _isDiscovering ? 
+            FittedBox(
+                  child: Container(
+                    margin: new EdgeInsets.all(16.0),
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ) :
+          IconButton(
+            icon: Icon(Icons.replay),
+            onPressed: (){
+              _startDiscovery();
+            },
+          )
+        ]
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("Paired Devices:"),
+          ),
+          Expanded(
+            child: ListView(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+              children: bondedEntries,
+            )
+          ),
+          Divider(),
+          ListTile(
+            title: Text("Unpaired Devices"),
+          ),
+          Expanded(
+            child: ListView(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+              children: unbondedEntries
+            )
+          )
+        ]
+      ),
+    );
+  }
+}
